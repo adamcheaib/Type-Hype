8575f4e1d27fcdbf3c68e4f9ba5ddfbae4fed81c
Adam <adam-cheaib@hotmail.com>
2023-04-07 03:46:30 +0200

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -3,5 +3,11 @@ const data = [
 
     `the vikings spoke old norse and made inscriptions in runes. For most of the period they followed the old Norse religion, but later became christians. the vikings had their own laws, art and architecture. most vikings were also farmers, fishermen, craftsmen and traders. popular conceptions of the vikings often strongly differ from the complex, advanced civilisation of the norsemen that emerges from archaeology and historical sources.`,
 
-    `The Inca Empire was the last chapter of thousands of years of Andean civilizations. The Andean civilization is one of at least five civilizations in the world deemed by scholars to be "pristine." The concept of a "pristine" civilization refers to a civilization that has developed independently from external influences and is not a derivative of other civilizations.`
+    `The Inca Empire was the last chapter of thousands of years of Andean civilizations. The Andean civilization is one of at least five civilizations in the world deemed by scholars to be "pristine." The concept of a "pristine" civilization refers to a civilization that has developed independently from external influences and is not a derivative of other civilizations.`,
+
+    "Development on Resident Evil 4 began for the PlayStation 2 in 1999. Four proposed versions were discarded, the first attempt was directed by Hideki Kamiya, but series creator Shinji Mikami felt it strayed too far from the series roots, so it was spun-off as Devil May Cry at his recommendation. Other versions were scrapped until Mikami took directorial duties for what became the final version.",
+
+    "TikTok, and its Chinese counterpart Douyin is a short-form video hosting service owned by the Chinese company ByteDance. It hosts user-submitted videos, which can range in duration from 3 seconds to 10 minutes. Since their launches, TikTok and Douyin have gained global popularity. In October 2020, TikTok surpassed 2 billion mobile downloads worldwide.",
+
+
 ];
